92b090ed03e74828b3ec13061d5302ab9fb748ab
Bao Huynh <hbaohgb@gmail.com>
2022-07-18 22:38:02 +0700

    update for deployment to eks

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac2e6929236bb4c23839eb32fdc060be-400281769.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
