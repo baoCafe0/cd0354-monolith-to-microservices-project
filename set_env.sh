3e66047073f5baaec4ee6c2265e6000fb54caffc
Bao Huynh <hbaohgb@gmail.com>
2022-12-13 19:57:17 +0700

    update for deployment

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=p3baohgproject
-export POSTGRES_HOST=p3baohgproject.ctv1sbnwx5fg.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::p3-baohg-bucket
+export POSTGRES_USERNAME=baohgproject3
+export POSTGRES_PASSWORD=baohgproject3
+export POSTGRES_HOST=baohgproject3.ctv1sbnwx5fg.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=baohgproject3
+export AWS_BUCKET=arn:aws:s3:::baohg-bucket-t12
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
